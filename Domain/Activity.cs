65ffb0d1a794c1dfd7efd327d1877198065aaa93
icafeda <icafeda@outlook.com>
2026-08-11 23:45:57 +0700

    backend railway domain updated

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -7,12 +7,9 @@ public class Activity
 {
     [Key]
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    [Required]
     public required string Title { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    [Required]
     public required string Description { get; set; } = string.Empty;
-    [Required]
     public required string Category { get; set; } = string.Empty;
     public bool IsCancelled { get; set; }
 
